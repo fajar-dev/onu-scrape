77d537f08484dbb89bd8dd75b3ef9f927ff0e1d7
fajar-dev <fajarrivaldi2015@gmail.com>
2025-10-08 09:50:33 +0700

    chore: sync data source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [Metrics, Cgs],
     migrations: [],
